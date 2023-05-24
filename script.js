50aeb7f2039bc75eecab3bbc7e32d1546d1a4505
Abraham Fatoki <fatokiabraham@gmail.com>
2023-05-24 10:51:22 +0100

    add copyright date

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+const socialLink = document.querySelector(".social-link");
+
+
+const year = new Date().getFullYear()
+socialLink.textContent = year
+
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
