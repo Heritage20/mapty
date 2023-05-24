1d5afbdd4a65507fb9574aa1d0db86276632953c
Abraham Fatoki <fatokiabraham@gmail.com>
2023-05-24 11:45:06 +0100

    add map popup functionality

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ const socialLink = document.querySelector(".social-link");
 const year = new Date().getFullYear();
 socialLink.textContent = year;
 
-// ,10z?hl=pt-PT
-
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -33,10 +31,24 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxwidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     function () {
       alert("Could not get your position.");
