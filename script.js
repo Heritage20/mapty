f03f2f389355f51b12dccb29c5966d325012ac1a
Abraham Fatoki <fatokiabraham@gmail.com>
2023-05-24 17:20:15 +0100

    add local storage

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
+const header = document.querySelector(".header");
 const form = document.querySelector(".form");
+const formCloseBtn = document.querySelector(".form__btn_close");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
 const inputDistance = document.querySelector(".form__input--distance");
@@ -91,10 +93,20 @@ class App {
   #workouts = [];
 
   constructor() {
+    // get user's position
     this._getPosition();
+
+    // get data from local storage
+    this._getLocalStorage();
+
+    // attach event handlers
     form.addEventListener("submit", this._newWorkout.bind(this));
+
     inputType.addEventListener("change", this._toggleElevationField);
+
     containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
+
+    formCloseBtn.addEventListener("click", this._hideForm.bind(this));
   }
 
   _getPosition() {
@@ -124,12 +136,17 @@ class App {
 
     //   handling clicks on map
     this.#map.on("click", this._showForm.bind(this));
+
+    this.#workouts.forEach((work) => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
     this.#mapEvent = mapE;
     form.classList.remove("hidden");
     inputDistance.focus();
+    header.style.display = "none";
   }
 
   _hideForm() {
@@ -143,6 +160,8 @@ class App {
     form.style.display = "none";
     form.classList.add("hidden");
     setTimeout(() => (form.style.display = "grid"), 1000);
+
+    header.style.display = "grid";
   }
 
   _toggleElevationField() {
@@ -190,7 +209,6 @@ class App {
 
     // add new objects to workout array
     this.#workouts.push(workout);
-    console.log(this.#workouts);
 
     // display workout on map as marker
     this._renderWorkoutMarker(workout);
@@ -200,6 +218,9 @@ class App {
 
     // Hide form + clear input fields
     this._hideForm();
+
+    // set local storage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -286,7 +307,28 @@ class App {
     });
 
     // using public interface
-    workout.click();
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem("workouts", JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem("workouts"));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach((work) => {
+      this._renderWorkOut(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem("workouts");
+    location.reload();
   }
 }
 
