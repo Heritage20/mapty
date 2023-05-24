a97df1ba06df3e6c0ce3618880010adab3162cf1
Abraham Fatoki <fatokiabraham@gmail.com>
2023-05-24 14:32:59 +0100

    add workout, running, cycling and app classes

--- script.js
+++ script.js
@@ -15,11 +15,54 @@ const socialLink = document.querySelector(".social-link");
 const year = new Date().getFullYear();
 socialLink.textContent = year;
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
 
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // minutes
+  }
+}
 
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  // min/km
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  // km/hr
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cyc1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cyc1);
+
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -68,37 +111,47 @@ class App {
   }
 
   _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every((inp) => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every((inp) => inp > 0);
+
     e.preventDefault();
 
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
 
     switch (type) {
       case "running":
         const cadence = +inputCadence.value;
         if (
           !validInputs(distance, duration, cadence) ||
-          allPositive(distance, duration, cadence)
+          !allPositive(distance, duration, cadence)
         )
-          return alert("Inputs have to be positive numbers");
+          return alert("Inputs have to be positive numbers!");
 
-        const Workout = new Running();
+        workout = new Running([lat, lng], distance, duration, cadence);
+
         break;
 
       case "cycling":
         const elevation = +inputElevation.value;
         if (
           !validInputs(distance, duration, elevation) ||
-          allPositive(distance, duration)
+          !allPositive(distance, duration)
         )
           return alert("Inputs have to be positive numbers");
+        workout = new Cycling([lat, lng], distance, duration, elevation);
         break;
     }
 
+    this.#workouts.push(workout);
+    console.log(this.#workouts);
+
     // display marker
-    // console.log(this.#mapEvent);
-    const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
       .bindPopup(
@@ -112,11 +165,13 @@ class App {
       )
       .setPopupContent("Workout")
       .openPopup();
+
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
   }
 }
 
 const app = new App();
-
-const validInputs = (...inputs) => inputs.every((inp) => Number.isFinite(inp));
-
-const allPositive = (...inputs) => inputs.every((inp) => inp > 0);
